5bc2f39d0f4422ac5da141284775cd36ae42405d
Femi Osinowo <femiosinowo@gmail.com>
2017-02-08 18:36:51 -0500

    Update ruby_test.rb

--- ruby_test.rb
+++ ruby_test.rb
@@ -5,7 +5,7 @@ driver = Selenium::WebDriver.for :firefox
 driver.get "http://google.com"
 
 element = driver.find_element :name => "q"
-element.send_keys "Femi!"
+element.send_keys "Cheese!"
 element.submit
 
 puts "Page title is #{driver.title}"
