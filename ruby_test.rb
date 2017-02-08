791ee724bbbd45e41823bbf853efa15fcd32e531
Femi Osinowo <femiosinowo@gmail.com>
2017-02-08 18:51:31 -0500

    Update ruby_test.rb

--- ruby_test.rb
+++ ruby_test.rb
@@ -5,12 +5,12 @@ driver = Selenium::WebDriver.for :firefox
 driver.get "http://google.com"
 
 element = driver.find_element :name => "q"
-element.send_keys "Cheese!"
+element.send_keys "Fem"
 element.submit
 
 puts "Page title is #{driver.title}"
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait = Selenium::WebDriver::Wait.new(:timeout => 50)
 wait.until { driver.title.downcase.start_with? "cheese!" }
 
 puts "Page title is #{driver.title}"
