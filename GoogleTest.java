c1478596bb1bd14a15f3eee43efc0d66303db50e
Femi Osinowo <femiosinowo@gmail.com>
2017-02-09 03:52:56 -0500

    Create GoogleTest.java

---
+++ GoogleTest.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/**
+ * @author Gaurang_Shah
+ */
+public class GoogleTest {
+ private WebDriver driver;
+
+ @Test
+ public void verifySearch() {
+  driver = new FirefoxDriver();
+  driver.get("http://www.google.com/");
+  driver.quit();
+ }
+}
